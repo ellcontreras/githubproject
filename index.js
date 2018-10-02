852781184c81bc1895f80df079a3e0c252f4706f
marco lopez <marco.lopez@bbxmstudios.com>
2018-10-01 23:28:05 -0500

    primer commit del repo

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Media from './src/playlist/components/media';
+
+const app = document.getElementById('app')
+
+//const holaMundo = <h1>Hola Mundo!</h1>;
+render(<Media /> , app);
---
+++ src/playlist/components/media.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import './media.css';
+
+
+class Media extends React.Component {
+ constructor(props) {
+   super(props);
+    this.state = {
+     input: '',
+     items: [],
+     total: 0
+  };
+   this.handleInputChange = this.handleInputChange.bind(this);
+   this.handleSubmit = this.handleInputChange.bind(this);
+   //this.handleClick = this.handleClick.bind(this);
+}
+   handleInputChange = (event) => {
+    this.setState({input: event.target.value})
+   }
+   handleSubmit = (event) => {
+     event.preventDefault();
+     axios.get(`https://api.github.com/search/repositories?q=${this.state.value}`)
+       .then(res =>
+        console.log(res.data)
+      ).catch(err => {
+       alert('error')
+    });
+  }
+  render() {
+     let itemsDOM = this.state.items.map(items => {
+       <li key={items.name}>
+          {items.fullName}
+       </li>
+     });
+     return(
+         <div className="App">
+           <div className='form'>
+              <input  value={this.state.input}
+                      onChange={this.handleInputChange}/>
+              <button className= "button"
+                onClick = {this.handleSubmit}>
+                Search
+              </button>
+          </div>
+          <div className="results">
+             <h5>Total: {this.state.total}</h5>
+             <ul>
+               {itemsDOM}
+             </ul>
+          </div>
+        </div>
+      )
+    }
+}
+export default Media;
